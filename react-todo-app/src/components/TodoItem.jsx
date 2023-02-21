d357c032589ce87e5307492bb9884be9d19cf2b9
Marko Kermiciev <kermicievmarko@gmail.com>
2023-02-21 06:17:49 +0100

    setup part of the project

---
+++ react-todo-app/src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+const TodoItem = ({ itemProp, setTodos }) => {
+
+  const handleChange = (id) => {
+    console.log('clicked', id);
+  };
+
+  return (
+    <li>
+      <input type="checkbox" 
+       checked={itemProp.completed}
+       onChange={() => handleChange(itemProp.id)}/>
+      {itemProp.title}
+    </li>
+  );
+};
+export default TodoItem;
---
+++ react-todo-app/src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import TodoItem from './TodoItem';
+
+const TodosList = ({ todosProps, setTodos }) => {
+  return (
+    <ul>
+      {todosProps.map((todo) => (
+        <TodoItem key={todo.id} itemProp={todo} setTodos={setTodos} />
+      ))}
+    </ul>
+  );
+};
+export default TodosList;
